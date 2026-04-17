a9eaa226a6510af83047e05cc4a33103e68667f2
infosecwizardtest <infosecwizard.test@gmail.com>
2026-04-16 20:11:51 -0400

    fixed

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const text = "Security Research | Ethical Hacking | Threat Detection";
-let i = 0;
+const cards = document.querySelectorAll(".card");
 
-function typing(){
-if(i < text.length){
-document.getElementById("typing").innerHTML += text.charAt(i);
-i++;
-setTimeout(typing,50);
-}
-}
+cards.forEach(card => {
 
-typing();
+card.addEventListener("mousemove", e => {
+
+const rect = card.getBoundingClientRect();
+const x = e.clientX - rect.left;
+const y = e.clientY - rect.top;
+
+const centerX = rect.width / 2;
+const centerY = rect.height / 2;
+
+const rotateX = (y - centerY) / 15;
+const rotateY = (centerX - x) / 15;
+
+card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-8px)`;
+
+});
+
+card.addEventListener("mouseleave", () => {
+
+card.style.transform = "rotateX(0) rotateY(0) translateY(0)";
+
+});
+
+});
